55563418721cfb1d8c9f78df615326a43230e1ef
Curtis Ekstrom <code@fauxton.io>
2016-02-07 02:02:27 +0100

    Show git status in prompt
    The main portion of this code is stolen from the Hashrocket
    dotmatrix repo, with a minor color addition to show the branch as
    red (with the type of uncommitted change).

--- zsh/configs/prompt.zsh
+++ zsh/configs/prompt.zsh
@@ -1,8 +1,48 @@
-# modify the prompt to contain git branch name if applicable
-git_prompt_info() {
-  current_branch=$(git current-branch 2> /dev/null)
-  if [[ -n $current_branch ]]; then
-    echo " %{$fg_bold[green]%}$current_branch%{$reset_color%}"
+git_prompt_info () {
+  local g="$(command git rev-parse --git-dir 2>/dev/null)"
+  if [ -n "$g" ]; then
+    local r
+    local b
+    local d
+    local s
+    # Rebasing
+    if [ -d "$g/rebase-apply" ] ; then
+      if test -f "$g/rebase-apply/rebasing" ; then
+        r="|REBASE"
+      fi
+      b="$(command git symbolic-ref HEAD 2>/dev/null)"
+    # Interactive rebase
+    elif [ -f "$g/rebase-merge/interactive" ] ; then
+      r="|REBASE-i"
+      b="$(cat "$g/rebase-merge/head-name")"
+    # Merging
+    elif [ -f "$g/MERGE_HEAD" ] ; then
+      r="|MERGING"
+      b="$(command git symbolic-ref HEAD 2>/dev/null)"
+    else
+      if [ -f "$g/BISECT_LOG" ] ; then
+        r="|BISECTING"
+      fi
+      if ! b="$(command git symbolic-ref HEAD 2>/dev/null)" ; then
+        if ! b="$(command git describe --exact-match HEAD 2>/dev/null)" ; then
+          b="$(cut -c1-7 "$g/HEAD")..."
+        fi
+      fi
+    fi
+
+    # Dirty Branch
+    local newfile='?? '
+    if [ -n "$ZSH_VERSION" ]; then
+      newfile='\?\? '
+    fi
+    d=''
+    color='green'
+    s=$(command git status --porcelain 2> /dev/null)
+    [[ $s =~ "$newfile" ]] && color='red' && d+='+'
+    [[ $s =~ "M " ]] && color='red' && d+='*'
+    [[ $s =~ "D " ]] && color='red' && d+='-'
+
+    echo " %{$fg_bold[$color]%}${b##refs/heads/}$r$d%{$reset_color%}"
   fi
 }
 setopt promptsubst
