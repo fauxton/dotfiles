66b7e63560e4b74b93a88745e00ccd319cb29859
Curtis Ekstrom <code@fauxton.io>
2017-03-18 14:36:52 -0700

    Remove Postgres.app from $PATH

--- zsh/configs/post/path.zsh
+++ zsh/configs/post/path.zsh
@@ -12,6 +12,4 @@ PATH="$HOME/.local/bin:$PATH"
 # mkdir .git/safe in the root of repositories you trust
 PATH=".git/safe/../../bin:$PATH"
 
-PATH=$PATH:/Applications/Postgres.app/Contents/Versions/latest/bin
-
 export -U PATH
