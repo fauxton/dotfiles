46172dbbe9ab13d59968147200f782fbf1c17f90
Curtis Ekstrom <code@fauxton.io>
2018-01-22 19:47:32 +0100

    Add homebrew path to path config

--- zsh/configs/post/path.zsh
+++ zsh/configs/post/path.zsh
@@ -12,6 +12,9 @@ PATH="$HOME/.local/bin:$PATH"
 # mkdir .git/safe in the root of repositories you trust
 PATH=".git/safe/../../bin:$PATH"
 
+# Add homebrew path
+PATH=$PATH:/usr/local/bin
+
 PATH=$PATH:/Applications/Postgres.app/Contents/Versions/latest/bin
 
 export -U PATH
